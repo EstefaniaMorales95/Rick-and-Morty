0e200c40b04e57cf46a9efe80ad92d8c310112da
Estefa Morales <estefamorales1995@icloud.com>
2024-11-12 15:36:55 -0500

    rick and morty

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import useFetch from './hooks/useFetch';
+import CardInfo from './components/CardInfo';
+import ResidentsList from './components/ResidentsList';
+import Search from './components/Search';
+import './App.css';
+
+function App() {
+	const [location, setLocation] = useFetch();
+	const [locationId, setLocationId] = useState(1);
+	const [page, setPage] = useState(1);
+	const itemsPerPage = 5;
+
+	useEffect(() => {
+		setLocation(`https://rickandmortyapi.com/api/location/${locationId}`);
+	}, [locationId]);
+
+	const totalItems = location?.residents ? location.residents.length : 0;
+	const maxPage = Math.ceil(totalItems / itemsPerPage);
+
+	const currentPageItems = location?.residents
+		? location.residents.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+		: [];
+
+	const onPrev = () => {
+		if (page > 1) setPage(page - 1);
+	};
+	const onNext = () => {
+		if (page < maxPage) setPage(page + 1);
+	};
+
+	return (
+		<>
+			<div className="hero" />
+			<div className="container">
+				<Search setLocationId={setLocationId} />
+				<CardInfo location={location} />
+				<ResidentsList residents={currentPageItems} />
+			</div>
+			<br />
+			<div className="pagination">
+				<button
+					className="pagination__btn"
+					onClick={onPrev}
+					disabled={page === 1}
+				>
+					Anterior
+				</button>
+				<span className="pagination__pg">
+					Página {page} / {maxPage}
+				</span>
+				<button
+					className="pagination__btn"
+					onClick={onNext}
+					disabled={page === maxPage}
+				>
+					Siguiente
+				</button>
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/ResidentCard.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import useFetch from '../hooks/useFetch';
+import './ResidentCard.css';
+
+function ResidentCard({ url }) {
+	const [resident, setResident] = useFetch();
+
+	useEffect(() => {
+		setResident(url);
+	}, [url]);
+
+	const status = resident?.status.toLowerCase();
+	const statusIcon =
+		status === 'alive' ? '🟢' : status === 'dead' ? '🔴' : '⚫️';
+
+	return (
+		<div className="card">
+			<div className="card__image">
+				<img
+					className="card__image-img"
+					src={resident?.image}
+					alt={resident?.name}
+				/>
+				<span className="card__status">
+					{statusIcon}
+					{resident?.status}
+				</span>
+			</div>
+			<h3 className="card__name">{resident?.name}</h3>
+			<div className="card__info">
+				<span className="card__info-item">
+					<span className="card__info-label">Spacie</span>
+					{resident?.species}
+				</span>
+				<span className="card__info-item">
+					<span className="card__info-label">Origin</span>
+					{resident?.origin?.name}
+				</span>
+				<span className="card__info-item">
+					<span className="card__info-label">Eppoisodes where appear</span>
+					{resident?.episode?.length}
+				</span>
+			</div>
+		</div>
+	);
+}
+
+export default ResidentCard;
